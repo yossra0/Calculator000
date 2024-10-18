f955bc1cdac188af71a9fc579401ac073bca23d6
yossra0 <yossraaq08@gmail.com>
2024-10-18 23:42:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Simple Calculator");
+        Console.WriteLine("Enter first number:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter second number:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter an operator (+, -, *, /):");
+        char operation = Console.ReadLine()[0];
+
+        double result=0;
+
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+                case '/':
+                result = num1 / num2;
+                break;
+           
+        }
+
+        Console.WriteLine($"Result: {result}");
+    }
+}
